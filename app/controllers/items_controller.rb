cfc35ae6c3e8fe87f4cb9f4c50e87f4632bf4543
Anna Villarreal <154029176+AnnaVi11arrea1@users.noreply.github.com>
2025-01-13 23:22:16 +0000

    backdoor form working

--- app/controllers/items_controller.rb
+++ app/controllers/items_controller.rb
@@ -4,4 +4,15 @@ class ItemsController < ApplicationController
 
     render({ :template => "item_templates/list" })
   end
+
+  def insert_item
+    render({ :template => "item_templates/insert_form" })
+  end
+
+  def create_row
+    item = Item.new({ :link_url => params[:URL], :link_description => params[:description], :thumbnail_url => params[:thumbnail] })
+    item.save
+
+    redirect_to :controller => "items", :action => "index"
+  end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,8 @@
 Rails.application.routes.draw do
 
   get("/", { :controller => "items", :action => "index" })
+
+  get("/backdoor", { :controller => "items", :action => "insert_item" })
+
+  post("/backdoor", { :controller => "items", :action => "create_row" })
 end
